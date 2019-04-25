0f2d12a3645a22ce74436f3af8badca2780114bf
yangbin <yangbin@altamob.com>
2019-04-25 15:26:09 +0800

    cache auth

--- src/utils/checkAuth.ts
+++ src/utils/checkAuth.ts
@@ -2,7 +2,7 @@ import { LOCALSTORAGE_KEYS } from '@constants/index'
 
 export const checkAuth = (name: string, component: React.ReactNode): React.ReactNode => {
   const tarString = localStorage.getItem(LOCALSTORAGE_KEYS.AUTHTARGET)
-  const target = JSON.parse(tarString)
+  const target = JSON.parse(tarString) || {};
   if (name && name.includes('&')) {
     const names = name.split('&')
     const check = names.every(ele => target[ele])
